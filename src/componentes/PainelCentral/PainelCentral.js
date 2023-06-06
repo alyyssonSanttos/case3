61907ee0254f9135d95d33b8ce46e88b72c5478c
alyyssonSanttos <allyssonsanttos321@gmail.com>
2023-06-06 19:13:37 -0300

    Case3

---
+++ src/componentes/PainelCentral/PainelCentral.js
@@ -0,0 +1,13 @@
+import "./PainelCentral.css"
+
+const PainelCentral = () => {
+    return (
+        <section>
+            <h3>Muito Obrigado João!</h3>
+            <p>Agradecemos sua compra. Em breve você receberá um email com a confirmação a acompanhamento do seu pedido.</p>
+            <button>Voltar as Compras!</button>
+        </section>
+    )
+}
+
+export default PainelCentral
